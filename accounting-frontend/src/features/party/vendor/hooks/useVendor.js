37a7089da47d2415f1a4f789bf8b79fae150544d
crazyprash007 <prashantkr.sharma07@gmail.com>
2026-01-15 00:58:14 +0530

    Add global search and multi-select for customers and vendors

--- accounting-frontend/src/features/party/vendor/hooks/useVendor.js
+++ accounting-frontend/src/features/party/vendor/hooks/useVendor.js
@@ -1,8 +1,8 @@
-// src/features/customers/customer/hooks/useVendor.js
+// src/features/party/vendor/hooks/useVendor.js
 import createUseResource from "src/services/useResourceFactory";
 import vendorApi from "../api/vendor.api";
 
-const STORAGE_KEY = "munim_customers_v1_demo";
+const STORAGE_KEY = "munim_vendors_v1_demo";
 
 const useVendor = createUseResource(vendorApi, STORAGE_KEY);
 
